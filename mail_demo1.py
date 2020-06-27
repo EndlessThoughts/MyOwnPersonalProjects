7303143e8c862d8766dfe19e087b6366d6d6dc5d
EndlessThoughts <55556079+EndlessThoughts@users.noreply.github.com>
2020-06-27 15:05:27 -0400

    Update mail_demo1.py

--- mail_demo1.py
+++ mail_demo1.py
@@ -13,10 +13,9 @@ Hey, hows it going?
 
 reciever = email_address
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(email_address, email_password)
-print("Login success")
-server.sendmail(email_address, reciever, message)
+server = smtplib.SMTP('smtp.gmail.com', 587) #Initialize server to establish a gmail (server adress) using port 587
+server.starttls() #Starts the connection
+server.login(email_address, email_password) #Login to the server
+print("Login success") 
+server.sendmail(email_address, reciever, message) #Send the email
 print("Message sent!")
-
